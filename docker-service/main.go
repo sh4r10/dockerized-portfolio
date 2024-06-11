04ebe538920c87148453d6ca0f0d26983b1b3070
Shariq Shahbaz <iamsh4r10@gmail.com>
2024-06-11 17:40:41 +0200

    create boilerplate frontend and boilerplate go docker service

---
+++ docker-service/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	apiClient, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+	defer apiClient.Close()
+
+	containers, err := apiClient.ContainerList(context.Background(), container.ListOptions{All: true})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, ctr := range containers {
+		fmt.Printf("%s\t%s\t%s\n", ctr.ID, ctr.Image, ctr.Names[0])
+	}
+}
